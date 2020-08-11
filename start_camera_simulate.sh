41d046eac9e4adb8837d1ad97b43e3ee13a0a8ea
MecaCho <1099833328@qq.com>
2020-08-11 23:51:25 +0800

    renew start_camera_simulate.sh;

--- start_camera_simulate.sh
+++ start_camera_simulate.sh
@@ -1,1 +1,1 @@
-ffpmeg -i /tmp/test.mp4 -stream_loop -1 -rtsp_transport tcp -vcodec h264 -f rtsp rtsp://119.3.27.38:554/test.sdp
+ffpmeg -i /tmp/test.mp4 -stream_loop -1 -rtsp_transport tcp -vcodec h264 -f rtsp rtsp://127.0.0.1:554/test.sdp
